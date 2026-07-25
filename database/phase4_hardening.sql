10909ed3f137b0a2a6fccfb94d6d9400ffcb4ec1
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-24 22:15:04 -0700

    Add recipe completion idempotency migration

---
+++ database/phase4_hardening.sql
@@ -0,0 +1,12 @@
+SET NAMES utf8mb4;
+SET time_zone = '+00:00';
+
+ALTER TABLE recipe_runs
+    ADD COLUMN completion_key CHAR(64) NULL AFTER prepared_by_member_id,
+    ADD UNIQUE KEY uq_recipe_runs_household_completion (household_id, completion_key);
+
+CREATE INDEX idx_recipe_ingredients_inventory
+    ON recipe_ingredients (inventory_item_id, recipe_id);
+
+CREATE INDEX idx_meal_plan_items_plan_date
+    ON meal_plan_items (meal_plan_id, meal_date, meal_type);
